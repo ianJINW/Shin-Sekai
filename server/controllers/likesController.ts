e407c8e1fa52c29a9356b4756bdbd4c1763410b1
loboo34 <rayovers40@gmail.com>
2025-10-19 21:12:13 +0300

    update to likes controller

--- server/controllers/likesController.ts
+++ server/controllers/likesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Post from "../models/postModel";
 import Like from "../models/likesModel";
 import Comment from "../models/commentModel";
+import Anime from "../models/animeModel";
 
 export const likeItem = async (req: Request, res: Response) => {
   const { itemId, type, user } = req.body;
@@ -10,55 +11,59 @@ export const likeItem = async (req: Request, res: Response) => {
     const existingLike = await Like.findOne({ itemId, userId: user, type });
 
     if (existingLike) {
-       res.status(400).json({ message: "Already liked" });
+      res.status(400).json({ message: "Already liked" });
     }
 
     const newLike = new Like({ itemId, userId: user, type });
     await newLike.save();
 
-    if (type == "post"){
-        await Post.findByIdAndUpdate(itemId, { $inc: { likesCount: 1 } });
-    } else if (type == "comment"){
-        await Comment.findByIdAndUpdate(itemId, { $inc: { likesCount: 1 } });
+    if (type == "post") {
+      await Post.findByIdAndUpdate(itemId, { $inc: { likesCount: 1 } });
+    } else if (type == "comment") {
+      await Comment.findByIdAndUpdate(itemId, { $inc: { likesCount: 1 } });
+    } else if (type == "anime") {
+      await Anime.findByIdAndUpdate(itemId, { $inc: { likesCount: 1 } });
     }
     res.json({ message: "Liked successfully" });
-
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const unlikeItem = async (req: Request, res: Response) => {
-    const { itemId, type, user } = req.body;
-    try{
-        const existingLike = await Like.findOne({ itemId, userId: user, type });
+  const { itemId, type, user } = req.body;
+  try {
+    const existingLike = await Like.findOne({ itemId, userId: user, type });
 
-        if (!existingLike) {
-             res.status(400).json({ message: "Not liked yet" });
-        }
-
-        await Like.deleteOne({ itemId, userId: user, type });
-
-        if (type == "post"){
-            await Post.findByIdAndUpdate(itemId, { $inc: { likesCount: -1 } });
-        } else if (type == "comment"){
-            await Comment.findByIdAndUpdate(itemId, { $inc: { likesCount: -1 } });
-        }
-        res.json({ message: "Unliked successfully" });
-    
-    } catch(err) {
-        res.status(500).json({ message: "Internal server error" });
+    if (!existingLike) {
+      res.status(400).json({ message: "Not liked yet" });
     }
-}
+
+    await Like.deleteOne({ itemId, userId: user, type });
+
+    if (type == "post") {
+      await Post.findByIdAndUpdate(itemId, { $inc: { likesCount: -1 } });
+    } else if (type == "comment") {
+      await Comment.findByIdAndUpdate(itemId, { $inc: { likesCount: -1 } });
+    } else if (type == "anime") {
+      await Anime.findByIdAndUpdate(itemId, {
+        $inc: {
+          likesCount: -1,
+        },
+      });
+    }
+    res.json({ message: "Unliked successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const getLikes = async (req: Request, res: Response) => {
-    const { itemId, type } = req.body;
-    try {
-        const likes = await Like.find({ itemId, type });
-        res.status(200).json(likes);
-    } catch (err) {
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
-
-
+  const { itemId, type } = req.body;
+  try {
+    const likes = await Like.find({ itemId, type });
+    res.status(200).json(likes);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
